a8faa03025d8613d6c35239af6af5c7352422e65
djBrug <d-brugmann@hotmail.com>
2015-05-11 20:31:05 -0500

    Please Work

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -21,7 +21,7 @@ class ReportsController < ApplicationController
 
     #@report.date = Date.strptime("05/15/2015", "%m/%d/%Y")
 
-    @report.date = "05/15/2015"
+    @report.date = params[:date]
 
     respond_to do |format|
       format.html # new.html.erb
